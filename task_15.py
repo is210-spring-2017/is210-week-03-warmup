16394a22b184bd4d6ec64f93325b14f1b1d312ae
javiguillen74 <javier.guillen@sps.cuny.edu>
2019-02-09 13:49:49 -0500

    Delete task_15.py

--- task_15.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""Provides variables for string and integer conversion."""
-
-
-NOT_THE_QUESTION = 'The answer to life, the universe, and everything? It\'s '
-ANSWER = 42
