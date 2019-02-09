e6560849748f89adcc4c6186021d2694649234a6
javiguillen74 <javier.guillen@sps.cuny.edu>
2019-02-09 13:48:48 -0500

    Delete task_05.py

--- task_05.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""Splinter would be proud."""
-
-
-TEENAGE_MUTANT_NINJAS = ('Michaelangelo. Leonardo. Rafael. Donatello. Heroes '
-                         'in a half shell.')
